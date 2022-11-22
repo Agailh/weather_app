43e1a3ad86d8807e76014b152cd32c8de170234d
Agailh <agailham86@gmail.com>
2022-11-22 13:16:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      api_key: "e134ee0a9dfb160c177e25097b317156",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="id">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="search.." />
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+#app {
+  background-image: url("./assets/1208599.png");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.5s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  /* background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75)); */
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
